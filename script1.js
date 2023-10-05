a8a072379abeed5f3fde42c52e22537fe6baa798
jimmy Daume <jimmy-lee.daume@docc.techstarter.de>
2023-10-05 15:53:12 +0200

    Tag1

---
+++ script1.js
@@ -0,0 +1,7 @@
+const welcomeMessage = "Willkommen auf unserer Cloud Engineering und DevOps Webseite!";
+const header = document.querySelector('h1');
+
+header.addEventListener('click', () => {
+    alert(welcomeMessage);
+});
+ //// 
